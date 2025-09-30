d4467e300406ee8220ae2fe24d53a8b3d9e32bc3
DecemberHB <27hyobin@gmail.com>
2025-09-30 17:31:59 +0900

    hyobin

--- src/main/java/kr/co/greengarden/controller/cs/NoticeController.java
+++ src/main/java/kr/co/greengarden/controller/cs/NoticeController.java
@@ -2,7 +2,11 @@ package kr.co.greengarden.controller.cs;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+/*
+ * 날짜 : 2025/09/26
+ * 이름 : 박효빈
+ * 내용 : NoticeController 추가 연결
+ */
 @Controller
 public class NoticeController {
 
