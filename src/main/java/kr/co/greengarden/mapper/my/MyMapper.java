a40e114c9475cd220038fcc51237b6185b81e7ae
sho5223 <sho5223@naver.com>
2025-10-16 16:53:34 +0900

    my 모달창--완료작업중

--- src/main/java/kr/co/greengarden/mapper/my/MyMapper.java
+++ src/main/java/kr/co/greengarden/mapper/my/MyMapper.java
@@ -9,4 +9,12 @@ import java.util.List;
 @Mapper
 public interface MyMapper {
     List<OrderSummaryDTO> selectRecentOrders(@Param("memId") String memId);
+    // ✅ 상태 업데이트 쿼리 4종 추가
+    void updateConfirmYn(@Param("orderNo") String orderNo, @Param("yn") String yn);
+
+    void updateReviewYn(@Param("orderNo") String orderNo, @Param("yn") String yn);
+
+    void updateExchangeYn(@Param("orderNo") String orderNo, @Param("yn") String yn);
+
+    void updateReturnYn(@Param("orderNo") String orderNo, @Param("yn") String yn);
 }
