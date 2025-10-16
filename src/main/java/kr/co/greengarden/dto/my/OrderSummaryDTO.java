995d29d3ab96de7e9e68db34b6f44ee9d37b2baf
henu-1004 <157605344+henu-1004@users.noreply.github.com>
2025-10-16 16:57:37 +0900

    Update OrderSummaryDTO.java

--- src/main/java/kr/co/greengarden/dto/my/OrderSummaryDTO.java
+++ src/main/java/kr/co/greengarden/dto/my/OrderSummaryDTO.java
@@ -20,16 +20,7 @@ public class OrderSummaryDTO {
     private LocalDateTime orderedAt;// 주문일자
     private String sellerId;        // 판매자 ID
     private String sellerAddress;   // 판매자 주소
-<<<<<<< Updated upstream
-    private String sellerName;// TB_MEMBER_SELLER.COMPANY
-    private String reviewYn;
-    private String returnYn;
-    private String exchangeYn;
-
-=======
     private String sellerName;      // TB_MEMBER_SELLER.COMPANY
->>>>>>> Stashed changes
-
     // ✅ 10.16 추가된 상태 관리 컬럼
     private String confirmYn;       // 구매확정 여부 ('Y' or 'N')
     private String reviewYn;        // 리뷰작성 여부 ('Y' or 'N')
