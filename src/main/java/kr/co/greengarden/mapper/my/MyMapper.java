92112eefe48fd0fd103191d4215f33ecd78c0fca
sho5223 <sho5223@naver.com>
2025-10-17 11:42:11 +0900

    리뷰기능구현중

--- src/main/java/kr/co/greengarden/mapper/my/MyMapper.java
+++ src/main/java/kr/co/greengarden/mapper/my/MyMapper.java
@@ -1,6 +1,7 @@
 package kr.co.greengarden.mapper.my;
 
 import kr.co.greengarden.dto.my.OrderSummaryDTO;
+import kr.co.greengarden.dto.my.ProductReviewDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -12,9 +13,17 @@ public interface MyMapper {
     // ✅ 상태 업데이트 쿼리 4종 추가
     void updateConfirmYn(@Param("orderNo") String orderNo, @Param("yn") String yn);
 
-    void updateReviewYn(@Param("orderNo") String orderNo, @Param("yn") String yn);
+    void updateReviewYn(@Param("orderNo") String orderNo,
+                        @Param("proId") Long proId,
+                        @Param("yn") String yn);
 
     void updateExchangeYn(@Param("orderNo") String orderNo, @Param("yn") String yn);
 
     void updateReturnYn(@Param("orderNo") String orderNo, @Param("yn") String yn);
+
+    // ✅ 리뷰 등록
+    void insertProductReview(ProductReviewDTO reviewDTO);
+
+    // ✅ 내가 작성한 리뷰 조회
+    List<ProductReviewDTO> getMyReviews(@Param("memId") String memId);
 }
